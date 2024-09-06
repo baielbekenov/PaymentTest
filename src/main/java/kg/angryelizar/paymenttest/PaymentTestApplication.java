b8a6127e10b2abe9fcaed538b6a3d43d6cc068b7
Elizar Konovalov <conovalov.elizar@gmail.com>
2024-09-06 22:55:43 +0600

    Start commit

---
+++ src/main/java/kg/angryelizar/paymenttest/PaymentTestApplication.java
@@ -0,0 +1,13 @@
+package kg.angryelizar.paymenttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentTestApplication.class, args);
+    }
+
+}

e3906fc398148e44b889ebd5a52cf485b4a139ef
Elizar Konovalov <conovalov.elizar@gmail.com>
2024-09-07 11:56:34 +0600

    Implemented JwtAuthenticationFilter

---
+++ src/main/java/kg/angryelizar/paymenttest/config/JwtAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package kg.angryelizar.paymenttest.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kg.angryelizar.paymenttest.service.JwtService;
+import kg.angryelizar.paymenttest.service.impl.AuthUserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter  extends OncePerRequestFilter {
+    public static final String BEARER_PREFIX = "Bearer ";
+    public static final String HEADER_NAME = "Authorization";
+    private final AuthUserDetailsServiceImpl authUserDetailsService;
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var authHeader = request.getHeader(HEADER_NAME);
+        if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, BEARER_PREFIX)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        var jwt = authHeader.substring(BEARER_PREFIX.length());
+        var username = jwtService.extractUsername(jwt);
+
+        if (StringUtils.isNotEmpty(username) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = authUserDetailsService.loadUserByUsername(username);
+
+            if (Boolean.TRUE.equals(jwtService.isTokenValid(jwt, userDetails))) {
+                SecurityContextHolder contextHolder = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                contextHolder.setAuthentication(authentication);
+                SecurityContextHolder.setContext(contextHolder);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
